9eb650ab2ec0f9145eaeda7c5cc9af533f6642d1
Kesha <aaaaa@mail.ru>
2021-12-29 16:40:58 +0300

    1 neuron

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ def sigmoid(x):
 class Neuron:
 
     def __init__(self, weights, bias):
-        self.weights = weights;
-        self.bias = bias;
+        self.weights = weights
+        self.bias = bias
 
     def feedforwar(self, inputs):
-        total = np.dot(self.weights, inputs) + self.bias;
-        return sigmoid(total);
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
 
 
-weights = np.array([2, 1]);
-bias = 5;
-n = Neuron(weights, bias);
+weights = np.array([2, 1])
+bias = 5
+n = Neuron(weights, bias)
 
-inputs = np.array([10, 12]);
-print(n.feedforwar(inputs));
-print("---------")
+inputs = np.array([10, 12])
+print(n.feedforwar(inputs))
